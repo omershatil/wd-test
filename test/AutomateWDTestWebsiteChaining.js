1c0807913eb786cd5737771a8d7cbd6f8d296edb
Omer Shatil <omershatil@gmail.com>
2015-12-29 22:41:24 +0100

    cleanup

--- test/AutomateWDTestWebsiteChaining.js
+++ test/AutomateWDTestWebsiteChaining.js
@@ -1,8 +1,6 @@
 /**
  * Created by omer on 12/23/2015.
  */
-var q = require('q');
-env = process.env;
 var conf = require('../util/config');
 
 describe("yield test", function () {
@@ -22,14 +20,13 @@ describe("yield test", function () {
     it('test1 should succeed', function () {
         // this code assumes no failures. In production code you should allow fail-and-recover promises,
         // as elements may not be visible or get stale and you still want to succeed, under certain conditions
-        console.log('\nSauceOnDemandSessionID=' + driver.sessionID + ' job-name=' + env.SAUCE_TEST_NAME);
         return driver.elementByXPath('//div[@id="i_am_an_id"]').then(function(el) {
         return el.click().then(function() {
         return driver.elementByXPath('//textarea[@id="comments"]').then(function(el) {
         return el.sendKeys('blah blah').then(function() {
         return driver.elementByXPath('//div[@id="i_am_an_id"]').then(function(el) {
         return el.click().then(null, function (err) {
-            console.log('do something!!!');
+        console.log('do something!!!');
         });});});});});});
     });
 });
